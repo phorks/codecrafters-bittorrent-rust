9e25aa21c21287d7d65bad8ea57414ce54dd2c7e
Pharaz Fadaei <pharaz.apps@gmail.com>
2024-10-02 12:01:23 +0330

    Implemented piece download

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use serde_json::{self, Map};
 use sha1::{Digest, Sha1};
 use std::{
     env, fs,
-    io::{Read, Write},
+    io::{Cursor, Read, Write},
     net::{Ipv4Addr, SocketAddrV4, TcpStream},
+    process::exit,
     str::FromStr,
 };
 
 const PEER_ID: &str = "00112233445566778899";
 const PROTOCOL_STRING: &str = "BitTorrent protocol";
+const BLOCK_SIZE: u32 = 1 << 14;
 
 // Available if you need it!
 // use serde_bencode
@@ -43,6 +45,30 @@ impl TorrentFile {
             addr: addr,
             file: self,
         }
+    }
+
+    fn find_peers(&self) -> impl Iterator<Item = Peer> {
+        let req = TrackerRequest {
+            info_hash: self.info.hash(),
+            peer_id: PEER_ID.into(),
+            port: 6881,
+            uploaded: 0,
+            downloaded: 0,
+            left: self.info.length,
+            compact: true,
+        };
+
+        let url = req.create_url(&self.announce);
+
+        let client = reqwest::blocking::Client::new();
+        let http_response = client.get(url).send().unwrap();
+
+        let resp: TrackerResponse =
+            serde_bencode::from_bytes(&http_response.bytes().unwrap()).unwrap();
+
+        resp.get_peers()
+            .into_iter()
+            .map(move |addr| Peer { addr, file: &self })
     }
 }
 
@@ -106,6 +132,7 @@ impl TrackerRequest {
 }
 
 #[derive(Deserialize)]
+#[allow(dead_code)]
 struct TrackerResponse {
     interval: u32,
     #[serde(rename = "peers", with = "serde_bytes")]
@@ -134,7 +161,7 @@ struct Peer<'a> {
 }
 
 impl<'a> Peer<'a> {
-    fn handshake(&self) -> Handshake {
+    fn handshake(&self) -> PeerConnection {
         let mut stream = TcpStream::connect(self.addr).unwrap();
 
         stream.write(&[PROTOCOL_STRING.len() as u8]).unwrap();
@@ -157,18 +184,154 @@ impl<'a> Peer<'a> {
         let mut peer_id = [0u8; 20];
         stream.read_exact(&mut peer_id).unwrap();
 
-        Handshake {
+        PeerConnection {
             protocol: pstring,
             info_hash,
             peer_id,
+            stream,
+            peer: self,
         }
     }
 }
 
-struct Handshake {
+#[allow(dead_code)]
+struct PeerConnection<'a> {
     protocol: Vec<u8>,
     info_hash: [u8; 20],
     peer_id: [u8; 20],
+    stream: TcpStream,
+    peer: &'a Peer<'a>,
+}
+
+impl<'a> PeerConnection<'a> {
+    fn receive_message(&mut self) -> PeerMessage {
+        let mut header = [0u8; 5];
+        self.stream.read_exact(&mut header).unwrap();
+
+        let length = Self::u32_from_bytes(&header);
+        let id = header[4];
+
+        let mut payload = vec![0u8; length as usize];
+        self.stream.read_exact(&mut payload).unwrap();
+
+        match id {
+            5 => PeerMessage::Bitfield,
+            2 => PeerMessage::Interested,
+            1 => PeerMessage::Unchoke,
+            6 => PeerMessage::Request(RequestPayload {
+                index: Self::u32_from_bytes(&payload),
+                begin: Self::u32_from_bytes(&payload[4..]),
+                length: Self::u32_from_bytes(&payload[8..]),
+            }),
+            7 => PeerMessage::Piece(PiecePayload {
+                index: Self::u32_from_bytes(&payload),
+                begin: Self::u32_from_bytes(&payload[4..]),
+                block: {
+                    payload.drain(0..8);
+                    payload
+                },
+            }),
+            _ => panic!("Unexpected message"),
+        }
+    }
+
+    fn send_message(&mut self, message: PeerMessage) {
+        match message {
+            PeerMessage::Interested => {
+                self.stream.write(&[2]).unwrap();
+            }
+            PeerMessage::Request(payload) => {
+                self.stream.write(&[6]).unwrap();
+                self.stream.write(&payload.index.to_be_bytes()).unwrap();
+                self.stream.write(&payload.begin.to_be_bytes()).unwrap();
+                self.stream.write(&payload.length.to_be_bytes()).unwrap();
+            }
+            _ => panic!("Unabled to send the message"),
+        };
+    }
+
+    fn download_piece(&mut self, index: u32, output: &str) {
+        let PeerMessage::Bitfield = self.receive_message() else {
+            panic!("Didn't receive the bitfield message")
+        };
+
+        self.send_message(PeerMessage::Interested);
+
+        let PeerMessage::Unchoke = self.receive_message() else {
+            panic!("Didn't receive the unchoke message")
+        };
+
+        let plength = self.peer.file.info.plength as u32;
+        let mut downloaded = 0;
+
+        let mut piece_data = Vec::<u8>::with_capacity(plength as usize);
+
+        while downloaded < plength {
+            let length = u32::min(BLOCK_SIZE, plength - downloaded);
+            self.send_message(PeerMessage::Request(RequestPayload {
+                index,
+                begin: downloaded,
+                length,
+            }));
+
+            let PeerMessage::Piece(payload) = self.receive_message() else {
+                panic!("Didn't receive the piece message")
+            };
+
+            let mut block_data = Cursor::new(payload.block);
+            std::io::copy(&mut block_data, &mut piece_data).unwrap();
+
+            downloaded += length;
+        }
+
+        let mut hasher = Sha1::new();
+        hasher.update(&piece_data);
+        let computed_hash = hasher.finalize();
+        let piece_hash = self
+            .peer
+            .file
+            .info
+            .get_pieces()
+            .nth(index as usize)
+            .unwrap();
+
+        if computed_hash.len() != piece_hash.len() {
+            panic!("Hash mismatch");
+        }
+
+        for i in 0..computed_hash.len() {
+            if computed_hash[i] != piece_hash[i] {
+                panic!("Hash mismatch");
+            }
+        }
+
+        fs::write(output, &piece_data).unwrap();
+    }
+
+    fn u32_from_bytes(data: &[u8]) -> u32 {
+        u32::from_be_bytes([data[0], data[1], data[2], data[3]])
+    }
+}
+
+enum PeerMessage {
+    Bitfield,
+    Interested,
+    Unchoke,
+    Request(RequestPayload),
+    Piece(PiecePayload),
+}
+
+struct RequestPayload {
+    index: u32,
+    begin: u32,
+    length: u32,
+}
+
+#[allow(dead_code)]
+struct PiecePayload {
+    index: u32,
+    begin: u32,
+    block: Vec<u8>,
 }
 
 fn bool_to_u8<S>(b: &bool, s: S) -> Result<S::Ok, S::Error>
@@ -178,7 +341,6 @@ where
     s.serialize_u8(if *b { 1 } else { 0 })
 }
 
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> Decode {
     // If encoded_value starts with a digit, it's a number
     let next = encoded_value.chars().next().unwrap();
@@ -263,32 +425,23 @@ fn main() {
         }
     } else if command == "peers" {
         let tfile = TorrentFile::from_file(&args[2]);
-        let req = TrackerRequest {
-            info_hash: tfile.info.hash(),
-            peer_id: PEER_ID.into(),
-            port: 6881,
-            uploaded: 0,
-            downloaded: 0,
-            left: tfile.info.length,
-            compact: true,
-        };
-
-        let url = req.create_url(&tfile.announce);
-
-        let client = reqwest::blocking::Client::new();
-        let http_response = client.get(url).send().unwrap();
-
-        let resp: TrackerResponse =
-            serde_bencode::from_bytes(&http_response.bytes().unwrap()).unwrap();
-
-        for peer in resp.get_peers() {
-            println!("{}", peer);
+        let peers = tfile.find_peers();
+        for peer in peers {
+            println!("{}", peer.addr);
         }
     } else if command == "handshake" {
         let tfile = TorrentFile::from_file(&args[2]);
         let peer = tfile.create_peer(SocketAddrV4::from_str(&args[3]).unwrap());
         let handshake = peer.handshake();
         println!("Peer ID: {}", hex::encode(handshake.peer_id));
+    } else if command == "download_piece" {
+        let output = &args[3];
+        let tfile = TorrentFile::from_file(&args[4]);
+        let piece = u32::from_str(&args[5]).unwrap();
+        let mut peers = tfile.find_peers();
+        let peer = peers.next().unwrap();
+        let mut connection = peer.handshake();
+        connection.download_piece(piece, output);
     } else {
         println!("unknown command: {}", args[1])
     }
